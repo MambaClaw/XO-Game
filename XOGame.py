936ce62b050846a1f34c931b3370f35d72d4320b
MambaClaw <nickidaiki@gmail.com>
2016-12-22 17:21:11 +0700

    plane can move

--- XOGame.py
+++ XOGame.py
@@ -8,15 +8,25 @@ class SpaceGameWindow(arcade.Window):
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.BLACK)
- 
-        self.ship = arcade.Sprite('images/assets/ship.png')
-        self.ship.set_position(100, 100)
- 
- 
+
+        i=0
+        for i in range(3):
+            j=0
+            for j in range(3):
+                self.ship = arcade.Sprite('images/assets/ship.png')
+                self.ship.set_position(200*i, 200*j)
+            
     def on_draw(self):
         arcade.start_render()
  
         self.ship.draw()
+
+    def animate(self, delta):
+        ship = self.ship
+ 
+        if ship.center_y > SCREEN_HEIGHT:
+            ship.center_y = 0
+        ship.set_position(ship.center_x, ship.center_y + 5)
  
  
 if __name__ == '__main__':
