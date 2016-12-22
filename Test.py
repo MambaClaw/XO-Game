2c704d241a68f3ae719f3d4ba07194db3a958f6d
MambaClaw <nickidaiki@gmail.com>
2016-12-22 23:11:50 +0700

    Test Another Way

---
+++ Test.py
@@ -0,0 +1,125 @@
+import arcade
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 120
+HEIGHT = 120
+
+# This sets the margin between each cell
+MARGIN = 1
+
+
+class MyApplication(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height):
+        self.TURN = 0
+        self.Player1Wins = False
+        self.Player2Wins = False
+        """
+        Set up the application.
+        """
+        super().__init__(width, height)
+        # Create a 2 dimensional array. A two dimensional
+        # array is simply a list of lists.
+        self.Matrix = []
+        for row in range(5):
+            # Add an empty array that will hold each cell
+            # in this row
+            self.Matrix.append([])
+            for column in range(5):
+                self.Matrix[row].append(0)  # Append a cell
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw the grid
+        
+        for row in range(5):
+            for column in range(5):
+                # Figure out what color to draw the box
+                if self.Matrix[row][column] == 1:
+                    color = arcade.color.GREEN
+                elif self.Matrix[row][column] == 2:
+                    color = arcade.color.YELLOW
+                else:
+                    color = arcade.color.WHITE
+
+                # Do the math to figure out where the box is
+                x = (MARGIN + WIDTH) * column + MARGIN + WIDTH // 2
+                y = (MARGIN + HEIGHT) * row + MARGIN + HEIGHT // 2
+
+                # Draw the box
+                arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, color)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        # Change the x/y screen coordinates to grid coordinates
+        column = x // (WIDTH + MARGIN)
+        row = y // (HEIGHT + MARGIN)
+
+        # Set that location to one
+        if(self.TURN == 0):
+            self.Matrix[row][column] = 1
+            self.TURN = 1
+        elif(self.TURN == 1):
+            self.Matrix[row][column] = 2
+            self.TURN = 0
+        print("Click coordinates: ({}, {}). Grid coordinates: ({}, {})"
+              .format(x, y, row, column))
+        print(self.TURN)
+
+        if(self.Matrix[0][0] == 1 and self.Matrix[0][1] == 1 and self.Matrix[0][2] == 1):
+            self.player1Wins = True
+        if(self.Matrix[1][0] == 1 and self.Matrix[1][1] == 1 and self.Matrix[1][2] == 1):
+            self.player1Wins = True
+        if(self.Matrix[2][0] == 1 and self.Matrix[2][1] == 1 and self.Matrix[2][2] == 1):
+            self.player1Wins = True
+        if(self.Matrix[0][0] == 1 and self.Matrix[1][0] == 1 and self.Matrix[2][0] == 1):
+            self.player1Wins = True
+        if(self.Matrix[0][1] == 1 and self.Matrix[1][1] == 1 and self.Matrix[2][1] == 1):
+            self.player1Wins = True
+        if(self.Matrix[0][2] == 1 and self.Matrix[1][2] == 1 and self.Matrix[2][2] == 1):
+            self.player1Wins = True
+        if(self.Matrix[0][0] == 1 and self.Matrix[1][1] == 1 and self.Matrix[2][2] == 1):
+            self.player1Wins = True
+        if(self.Matrix[0][2] == 1 and self.Matrix[1][1] == 1 and self.Matrix[2][0] == 1):
+            self.player1Wins = True
+            
+        if(self.Matrix[0][0] == 2 and self.Matrix[0][1] == 2 and self.Matrix[0][2] == 2):
+            self.player2Wins = True
+        if(self.Matrix[1][0] == 2 and self.Matrix[1][1] == 2 and self.Matrix[1][2] == 2):
+            self.player2Wins = True
+        if(self.Matrix[2][0] == 2 and self.Matrix[2][1] == 2 and self.Matrix[2][2] == 2):
+            self.player2Wins = True
+        if(self.Matrix[0][0] == 2 and self.Matrix[1][0] == 2 and self.Matrix[2][0] == 2):
+            self.player2Wins = True
+        if(self.Matrix[0][1] == 2 and self.Matrix[1][1] == 2 and self.Matrix[2][1] == 2):
+            self.player2Wins = True
+        if(self.Matrix[0][2] == 2 and self.Matrix[1][2] == 2 and self.Matrix[2][2] == 2):
+            self.player2Wins = True
+        if(self.Matrix[0][0] == 2 and self.Matrix[1][1] == 2 and self.Matrix[2][2] == 2):
+            self.player2Wins = True
+        if(self.Matrix[0][2] == 2 and self.Matrix[1][1] == 2 and self.Matrix[2][0] == 2):
+            self.player2Wins = True
+        if self.Player1Wins : print("Player1 Wins")
+        elif self.Player2Wins : print("Player2 Wins")
+        print (self.Matrix)
+
+
+window = MyApplication(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+arcade.run()
--- XOGame.py
+++ XOGame.py
@@ -5,7 +5,6 @@ SCREEN_HEIGHT = 600
 w = 3
 h = 3
 Matrix = [[0 for x in range(w)] for y in range(h)]
-Turn = 0
 class SpaceGameWindow(arcade.Window):
     WIDTH = 3
     LENGTH = 3
@@ -35,18 +34,17 @@ class SpaceGameWindow(arcade.Window):
         self.table02_sprite.set_position(500, 500)
         self.table12_sprite.set_position(500, 300)
         self.table22_sprite.set_position(500, 100)
-        
  
     def on_draw(self):
         arcade.start_render()
         self.table00_sprite.set_position(100, 500)
-        self.table01_sprite.set_position(100, 300)
-        self.table02_sprite.set_position(100, 100)
-        self.table10_sprite.set_position(300, 500)
+        self.table01_sprite.set_position(300, 500)
+        self.table02_sprite.set_position(500, 500)
+        self.table10_sprite.set_position(100, 300)
         self.table11_sprite.set_position(300, 300)
-        self.table12_sprite.set_position(300, 100)
-        self.table20_sprite.set_position(500, 500)
-        self.table21_sprite.set_position(500, 300)
+        self.table12_sprite.set_position(500, 300)
+        self.table20_sprite.set_position(100, 100)
+        self.table21_sprite.set_position(300, 100)
         self.table22_sprite.set_position(500, 100)
         self.table00_sprite.draw()
         self.table01_sprite.draw()
@@ -57,100 +55,113 @@ class SpaceGameWindow(arcade.Window):
         self.table20_sprite.draw()
         self.table21_sprite.draw()
         self.table22_sprite.draw()
-        st = input()
+        if True :
+            print("Player1's Turn")
+            st = input()
+            if st == 'q' :
+                if Matrix[0][0] == 0 : Matrix[0][0] = 1
+            elif st == 'w' :
+                if Matrix[0][1] == 0 : Matrix[0][1] = 1
+            elif st == 'e' :
+                if Matrix[0][2] == 0 : Matrix[0][2] = 1
+            elif st == 'a' :
+                if Matrix[1][0] == 0 : Matrix[1][0] = 1
+            elif st == 's' :
+                if Matrix[1][1] == 0 : Matrix[1][1] = 1
+            elif st == 'd' :
+                if Matrix[1][2] == 0 : Matrix[1][2] = 1
+            elif st == 'z' :
+                if Matrix[2][0] == 0 : Matrix[2][0] = 1
+            elif st == 'x' :
+                if Matrix[2][1] == 0 : Matrix[2][1] = 1
+            elif st == 'c' :
+                if Matrix[2][2] == 0 : Matrix[2][2] = 1
+            print (Matrix)
+        self.table00_sprite.draw()
+        self.table01_sprite.draw()
+        self.table02_sprite.draw()
+        self.table10_sprite.draw()
+        self.table11_sprite.draw()
+        self.table12_sprite.draw()
+        self.table20_sprite.draw()
+        self.table21_sprite.draw()
+        self.table22_sprite.draw()
+        self.table00_sprite.draw()
+        self.table01_sprite.draw()
+        self.table02_sprite.draw()
+        self.table10_sprite.draw()
+        self.table11_sprite.draw()
+        self.table12_sprite.draw()
+        self.table20_sprite.draw()
+        self.table21_sprite.draw()
+        self.table22_sprite.draw()
+        self.table00_sprite.draw()
+        self.table01_sprite.draw()
+        self.table02_sprite.draw()
+        self.table10_sprite.draw()
+        self.table11_sprite.draw()
+        self.table12_sprite.draw()
+        self.table20_sprite.draw()
+        self.table21_sprite.draw()
+        self.table22_sprite.draw()
+        self.table00_sprite.draw()
+        self.table01_sprite.draw()
+        self.table02_sprite.draw()
+        self.table10_sprite.draw()
+        self.table11_sprite.draw()
+        self.table12_sprite.draw()
+        self.table20_sprite.draw()
+        self.table21_sprite.draw()
+        self.table22_sprite.draw()
+        self.table00_sprite.draw()
+        self.table01_sprite.draw()
+        self.table02_sprite.draw()
+        self.table10_sprite.draw()
+        self.table11_sprite.draw()
+        self.table12_sprite.draw()
+        self.table20_sprite.draw()
+        self.table21_sprite.draw()
+        self.table22_sprite.draw()
         if(True):
-            if(st=='q'):
-                if(Matrix[0][0] == 0):
-                    Matrix[0][0] = 1
-                    P1Turn = False
-            elif(st=='w'):
-                if(Matrix[0][1] == 0):
-                    Matrix[0][1] = 1
-                    P1Turn = False
-            elif(st=='e'):
-                if(Matrix[0][2] == 0):
-                    Matrix[0][2] = 1
-                    P1Turn = False
-            elif(st=='a'):
-                if(Matrix[1][0] == 0):
-                    Matrix[1][0] = 1
-                    P1Turn = False
-            elif(st=='s'):
-                if(Matrix[1][1] == 0):
-                    Matrix[1][1] = 1
-                    P1Turn = False
-            elif(st=='d'):
-                if(Matrix[1][2] == 0):
-                    Matrix[1][2] = 1
-                    P1Turn = False
-            elif(st=='z'):
-                if(Matrix[2][0] == 0):
-                    Matrix[2][0] = 1
-                    P1Turn = False
-            elif(st=='x'):
-                if(Matrix[2][1] == 0):
-                    Matrix[2][1] = 1
-                    P1Turn = False
-            elif(st=='c'):
-                if(Matrix[2][2] == 0):
-                    Matrix[2][2] = 1
-                    P1Turn = False
-        if(True):
-            if(st=='u'):
+            print("Player2's Turn")
+            st = input()
+            if(st == 'u'):
                 if(Matrix[0][0] == 0):
                     Matrix[0][0] = 2
-                    P1Turn = True
-            elif(st=='i'):
+                    self.table00_sprite = arcade.Sprite('images/assets/x.png')
+            elif(st == 'i'):
                 if(Matrix[0][1] == 0):
                     Matrix[0][1] = 2
-                    P1Turn = True
-            elif(st=='o'):
+                    self.table01_sprite = arcade.Sprite('images/assets/x.png')
+            elif(st == 'o'):
                 if(Matrix[0][2] == 0):
                     Matrix[0][2] = 2
-                    P1Turn = True
-            elif(st=='h'):
+                    self.table02_sprite = arcade.Sprite('images/assets/x.png')
+            elif(st == 'h'):
                 if(Matrix[1][0] == 0):
                     Matrix[1][0] = 2
-                    P1Turn = True
-            elif(st=='j'):
+                    self.table10_sprite = arcade.Sprite('images/assets/x.png')
+            elif(st == 'j'):
                 if(Matrix[1][1] == 0):
                     Matrix[1][1] = 2
-                    P1Turn = True
-            elif(st=='k'):
+                    self.table11_sprite = arcade.Sprite('images/assets/x.png')
+            elif(st == 'k'):
                 if(Matrix[1][2] == 0):
                     Matrix[1][2] = 2
-                    P1Turn = True
-            elif(st=='b'):
+                    self.table12_sprite = arcade.Sprite('images/assets/x.png')
+            elif(st == 'b'):
                 if(Matrix[2][0] == 0):
                     Matrix[2][0] = 2
-                    P1Turn = True
-            elif(st=='n'):
+                    self.table20_sprite = arcade.Sprite('images/assets/x.png')
+            elif(st == 'n'):
                 if(Matrix[2][1] == 0):
                     Matrix[2][1] = 2
-                    P1Turn = True
-            elif(st=='m'):
+                    self.table21_sprite = arcade.Sprite('images/assets/x.png')
+            elif(st == 'm'):
                 if(Matrix[2][2] == 0):
                     Matrix[2][2] = 2
-                    P1Turn = True
-
-        if(Matrix[0][0] == 1):
-            self.table00_sprite = arcade.Sprite('images/assets/x.png')
-        if(Matrix[0][1] == 1):
-            self.table01_sprite = arcade.Sprite('images/assets/x.png')
-        if(Matrix[0][2] == 1):
-            self.table02_sprite = arcade.Sprite('images/assets/x.png')
-        if(Matrix[1][0] == 1):
-            self.table10_sprite = arcade.Sprite('images/assets/x.png')
-        if(Matrix[1][1] == 1):
-            self.table11_sprite = arcade.Sprite('images/assets/x.png')
-        if(Matrix[1][2] == 1):
-            self.table12_sprite = arcade.Sprite('images/assets/x.png')
-        if(Matrix[2][0] == 1):
-            self.table20_sprite = arcade.Sprite('images/assets/x.png')
-        if(Matrix[2][1] == 1):
-            self.table21_sprite = arcade.Sprite('images/assets/x.png')
-        if(Matrix[2][2] == 1):
-            self.table22_sprite = arcade.Sprite('images/assets/x.png')
+                    self.table22_sprite = arcade.Sprite('images/assets/x.png')
+            print (Matrix)
 
         if(Matrix[0][0] == 2):
             self.table00_sprite = arcade.Sprite('images/assets/o.png')
@@ -170,6 +181,52 @@ class SpaceGameWindow(arcade.Window):
             self.table21_sprite = arcade.Sprite('images/assets/o.png')
         if(Matrix[2][2] == 2):
             self.table22_sprite = arcade.Sprite('images/assets/o.png')
+            
+        self.table00_sprite.draw()
+        self.table01_sprite.draw()
+        self.table02_sprite.draw()
+        self.table10_sprite.draw()
+        self.table11_sprite.draw()
+        self.table12_sprite.draw()
+        self.table20_sprite.draw()
+        self.table21_sprite.draw()
+        self.table22_sprite.draw()
+        self.table00_sprite.draw()
+        self.table01_sprite.draw()
+        self.table02_sprite.draw()
+        self.table10_sprite.draw()
+        self.table11_sprite.draw()
+        self.table12_sprite.draw()
+        self.table20_sprite.draw()
+        self.table21_sprite.draw()
+        self.table22_sprite.draw()
+        self.table00_sprite.draw()
+        self.table01_sprite.draw()
+        self.table02_sprite.draw()
+        self.table10_sprite.draw()
+        self.table11_sprite.draw()
+        self.table12_sprite.draw()
+        self.table20_sprite.draw()
+        self.table21_sprite.draw()
+        self.table22_sprite.draw()
+        self.table00_sprite.draw()
+        self.table01_sprite.draw()
+        self.table02_sprite.draw()
+        self.table10_sprite.draw()
+        self.table11_sprite.draw()
+        self.table12_sprite.draw()
+        self.table20_sprite.draw()
+        self.table21_sprite.draw()
+        self.table22_sprite.draw()
+        self.table00_sprite.draw()
+        self.table01_sprite.draw()
+        self.table02_sprite.draw()
+        self.table10_sprite.draw()
+        self.table11_sprite.draw()
+        self.table12_sprite.draw()
+        self.table20_sprite.draw()
+        self.table21_sprite.draw()
+        self.table22_sprite.draw()
             
     def calculate(self, delta):
         if(P1Turn):
@@ -246,7 +303,7 @@ class SpaceGameWindow(arcade.Window):
                 if(Matrix[2][2] == 0):
                     Matrix[2][2] = 2
                     P1Turn = True
-
+        
     def checkTable(self, delta):
         if(Matrix[0][0] == 1 and Matrix[0][1] == 1 and Matrix[0][2] == 1):
             player1Wins = True
@@ -281,14 +338,7 @@ class SpaceGameWindow(arcade.Window):
             player2Wins = True
         if(Matrix[0][2] == 2 and Matrix[1][1] == 2 and Matrix[2][0] == 2):
             player2Wins = True
-
-class Table:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
- 
-    def animate(self, delta):
-        pass
+        
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
